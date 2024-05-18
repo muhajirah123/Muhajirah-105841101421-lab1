c3a2682f2c6c904c5e2c729ab52a03d7634460bd
muhajirah123 <105841101421@student.unismuh.ac.id>
2024-05-18 12:28:51 +0800

    tugas 1

--- App.js
+++ App.js
@@ -3,18 +3,13 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{flex:1,flexDirection: 'column'}}>
+        <View style={{flex:1,backgroundColor: 'red'}}>
+        </View>
+        <View style={{flex:1,backgroundColor: 'blue'}}>
+    </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
